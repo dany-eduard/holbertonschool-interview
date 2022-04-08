bacb6c15ed0ae66c1d178ffd45c978b8e32b7a9d
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2022-04-08 18:45:57 -0500

    fix: task

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,27 @@
 #!/usr/bin/node
-const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
+const request = require('request');
 
-async function starwarsCharacters(filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
+const movieId = process.argv[2];
 
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
+const displayNamesAsync = async char => {
+  return new Promise((resolve, reject) => {
+    request(char, (error, response, body) => {
+      if (response) {
+        resolve(JSON.parse(body).name);
+      } else if (error) {
+        reject(error);
+      }
+    });
+  });
+};
+
+request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response) {
+    const characters = JSON.parse(body).characters;
+    for (const char of characters) {
+      console.log(await displayNamesAsync(char));
+    }
   }
-}
-
-starwarsCharacters(filmID);
+});
