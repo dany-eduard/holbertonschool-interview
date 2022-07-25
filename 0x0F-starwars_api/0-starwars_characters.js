9883d769e24ec2394d19c83d41160345accd6c56
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2022-07-25 08:04:49 -0500

    fix: tasks

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,36 +1,23 @@
 #!/usr/bin/node
-const argv = process.argv;
-const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
-const urlMovie = `${urlFilm}${argv[2]}/`;
-
 const request = require('request');
+const movieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
-request(urlMovie, function (error, response, body) {
-  if (error == null) {
-    const fbody = JSON.parse(body);
-    const characters = fbody.characters;
-
-    if (characters && characters.length > 0) {
-      const limit = characters.length;
-      CharRequest(0, characters[0], characters, limit);
-    }
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
   } else {
-    console.log(error);
+    const characters = JSON.parse(body).characters;
+    getNames(characters, 0);
   }
 });
 
-function CharRequest (idx, url, characters, limit) {
-  if (idx === limit) {
-    return;
-  }
-  request(url, function (error, response, body) {
-    if (!error) {
-      const rbody = JSON.parse(body);
-      console.log(rbody.name);
-      idx++;
-      CharRequest(idx, characters[idx], characters, limit);
-    } else {
-      console.error('error:', error);
-    }
+function getNames (characters, check) {
+  if (check >= characters.length) { return; }
+
+  request.get(characters[check], (err, res, body) => {
+    if (err) { console.error(err); } else { console.log(JSON.parse(body).name); }
+
+    getNames(characters, check + 1);
   });
 }
